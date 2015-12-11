df7a801ec8de048c8a0bba52fa15d9922e21d96b
Nick Pfundstein <npfund@agileoasis.com>
2015-12-11 16:45:33 -0500

    Now executing opcodes 0, 19 and 21

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-extern crate bincode;
-use bincode::rustc_serialize::encode;
-use bincode::SizeLimit;
-
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
+use std::char;
 
 fn main() {
     let bytes = read_file("challenge.bin");
     
-    let mut file = File::create("out").unwrap();
-    for pair in bytes.iter() {
-        file.write_all(&encode(pair, SizeLimit::Infinite).unwrap());
+    let mut index = 0;
+    while index < bytes.len() {
+        let instruction = bytes[index];
+        match instruction {
+            0 => break,
+            19 => {
+                print!("{}", char::from_u32(bytes[index + 1] as u32).unwrap());
+                index += 1;
+            },
+            21 => index += 1,
+            _ => {
+                //println!("Unknown instruction {}", instruction);
+                index += 1
+            },
+        }
     }
 }
 
@@ -21,7 +29,7 @@ fn read_file(file: &str) -> Vec<u16> {
 
     let mut buffer = [0; 2];
     while file.read(&mut buffer).unwrap() > 0 {
-        let value = ((buffer[0] as u16) << 8) | buffer[1] as u16;
+        let value = ((buffer[1] as u16) << 8) | buffer[0] as u16;
         bytes.push(value);
     }
 
